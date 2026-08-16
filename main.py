73d60d8dab9a8e1feae99032501076e19793a97d
Michael_Kim <michael.kim1002@gmail.com>
2026-08-16 13:42:38 -0500

    edit main

--- main.py
+++ main.py
@@ -37,20 +37,15 @@ def load_user(user_id):
 
 class Base(DeclarativeBase):
     pass
-
 database_url = os.environ.get("DATABASE_URL")
 
 if database_url:
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
+else:
+    database_url = "sqlite:///tasks.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
-app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
-    "connect_args": {
-        "sslmode": "require"
-    }
-}
-
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
